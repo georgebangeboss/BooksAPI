875672b8d2e922f7207e2dbf5c31e1581bf10a5e
George <bangegeorgeogeto@gmail.com>
2023-10-10 15:53:47 +0300

    first commit

---
+++ books/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)  
+    num_of_pages=models.IntegerField()
+    date_of_publication =models.DateField(default='1999-10-10')
+
+    class Meta:
+        db_table='books'
+
+        
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    
+
+    class Meta:
+        db_table='authors'
+
+    
+
+
